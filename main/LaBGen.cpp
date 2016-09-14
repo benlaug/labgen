117d4df40d44685692edecb4d200545f387eb343
Benjamin Laugraud <blaugraud@ulg.ac.be>
2016-09-14 16:51:39 +0200

    Correction of a mistake in the copyright

--- main/LaBGen.cpp
+++ main/LaBGen.cpp
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with BGSLibrary.  If not, see <http://www.gnu.org/licenses/>.
+ * along with LaBGen.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <algorithm>
 #include <cstddef>
